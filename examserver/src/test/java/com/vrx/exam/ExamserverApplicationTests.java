c8ea69e6255a75a33f94b5fa817a119034e978df
Vijay Kumar <71895936+vijayd8055@users.noreply.github.com>
2021-10-11 01:06:55 +0530

    initial code

---
+++ examserver/src/test/java/com/vrx/exam/ExamserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrx.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
